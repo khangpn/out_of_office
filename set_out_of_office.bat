e5f3ad50adcb26b4f17733f5fc367d4c5ac18a6e
Khang Nguyen <khang.nguyen@arag.be>
2018-12-05 16:22:18 +0100

    Add hta bat ps1 - User input data on hta. it will be passed to bat as an interface to ps1

---
+++ set_out_of_office.bat
@@ -0,0 +1,24 @@
+echo off
+REM This file launches an Exchange powershell script
+REM To run this script as a scheduled task a domain user which is member of the appropriate Exchange management groups should be used.
+REM Depending on the needed rights make the user member of 'Organization Management' group or less.
+REM The user does not need to be member of 'Domain Admins' or Administrators groups.
+
+set psScript=%1
+set mailBox=%2
+set externalMessage=%3
+set internalMessage=%4
+set startDate=%5
+set endDate=%6
+
+echo %psScript%
+echo %mailBox%
+echo %externalMessage%
+echo %internalMessage%
+echo %startDate%
+echo %endDate%
+
+REM C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -version 2.0 -command ". 'C:\Program Files\Microsoft\Exchange Server\V14\bin\RemoteExchange.ps1'; Connect-ExchangeServer -auto; C:\Scripts\setOutOfOffice\set-OutOfOffice.ps1"
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -version 2.0 -executionpolicy unrestricted -command ". 'C:\Program Files\Microsoft\Exchange Server\V14\bin\RemoteExchange.ps1'; Connect-ExchangeServer -auto;" 
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -version 2.0 -executionpolicy unrestricted -command ". '%psScript%' -mailBox '%mailBox%' -externalMessage '%externalMessage%' -internalMessage '%internalMessage%' -startDate '%startDate%' -endDate '%endDate%'"
+PAUSE
---
+++ set_out_of_office.ps1
@@ -0,0 +1,19 @@
+﻿param(
+  [string]$mailBox,
+  [string]$startDate,
+  [string]$endDate,
+  [string]$externalMessage,
+  [string]$internalMessage
+)
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+. $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+Connect-ExchangeServer -auto
+
+Write-Host $mailBox
+Write-Host $startDate
+Write-Host $endDate
+Write-Host $externalMessage
+Write-Host $internalMessage
+
+set-mailboxautoreplyconfiguration $mailBox -AutoReplyState scheduled -starttime $startTime -endtime $endTime -externalmessage $externalMessage -internalmessage $internalMessage
